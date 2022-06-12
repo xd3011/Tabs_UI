34599d93f7b54dd65956d7a69296faf892d5f192
xd3011 <xuandat3011@gmail.com>
2022-06-13 00:13:22 +0700

    Update Effect Tabs UI

--- main.js
+++ main.js
@@ -4,4 +4,20 @@ const $$ = document.querySelectorAll.bind(document)
 const tabs = $$('.tab-item')
 const panes = $$('.tab-pane')
 
-console.log(tabs, panes)
+const tabActive = $('.tab-item.active')
+const tabLine = $('.tabs .line')
+
+tabLine.style.left = tabActive.offsetLeft + 'px'
+tabLine.style.width = tabActive.offsetWidth + 'px' 
+
+tabs.forEach((tab, index) => {
+    const pane = panes[index]
+    tab.onclick = function() {
+        $('.tab-item.active').classList.remove('active')
+        $('.tab-pane.active').classList.remove('active')
+        this.classList.add('active')
+        pane.classList.add('active')
+        tabLine.style.left = this.offsetLeft + 'px'
+        tabLine.style.width = this.offsetWidth + 'px'
+    }
+})
